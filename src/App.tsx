152b6b6923b13529c15f529a8f74c3e97a0a2096
Matthew McParland <matthewmcparland@Matthews-MBP.lan>
2020-02-03 12:15:35 +0000

    feat: adding linting, prettier and component lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and saver to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Rosie smells.
+      </p>
+    </header>
+  </div>
+);
 
 export default App;
