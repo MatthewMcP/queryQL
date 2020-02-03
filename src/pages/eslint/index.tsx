1eaeeba05523808094ee9150f8b7593df6449365
Matthew McParland <matthewmcparland@Matthews-MBP.lan>
2020-02-03 12:32:18 +0000

    fix: temp fix for linting error

--- src/pages/eslint/index.tsx
+++ src/pages/eslint/index.tsx
@@ -1,3 +1,8 @@
+declare var React: any;
+var React;
+import React from "react";
+import * as React from "react";
+
 const eslint = () => (
   // TODO: Add this component when tidying up linting rules
   <div>TODO</div>
