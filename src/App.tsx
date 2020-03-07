b459369e2435e1bd1cb0ce88e22704605808c01c
Matthew McParland <matthewmcparland@Matthews-MBP.lan>
2020-03-07 11:44:52 +0000

    feat: adding eslint, preitter etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Link } from "@reach/router"
-import ScoreLanding from "./pages/Scores/ScoreLanding"
+import React from "react";
+import "./App.css";
+import ScoreLanding from "./pages/Scores/ScoreLanding";
 
 const App = () => (
   <div className="App">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
 
     {/* <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
